0b023fb301498917539258bf180eb72381121117
Zixiang Kan <zk2244.columbia.edu>
2021-04-10 15:50:40 +0000

    Added simplified stats html

--- project/sightings/urls.py
+++ project/sightings/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
         path('sightings/',views.index),
+        path('sightings/detail/',views.detail, name='detail'),
+        path('sightings/stats/',views.stats, name='stats'),
         ]
 
--- project/sightings/views.py
+++ project/sightings/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.shortcuts import get_object_or_404
 from .models import Squirrel
 
 # Create your views here.
@@ -9,10 +9,56 @@ def index(request):
 
     squirrels = Squirrel.objects.all()
     context = {
-            'squirrels':squirrels
+            'squirrels':squirrels,
             }
 
+    
     return render(request, 'sightings/index.html',context)
+
+def detail(request,unique_squirrel_id):
+    
+    squirrel = get_object_or_404(Squirrel, pk = unique_squirrel_id)
+    context = {
+            'squirrel':squirrel,
+            }
+
+    return render(request, 'sightings/detail.html',context)
+
+def stats(request):
+    #get all squirrels
+    squirrels = Squirrel.objects.all()
+    #total number
+    total = squirrels.count()
+    #age
+    Adult_n = squirrels.filter(age='Adult').count()
+    Juvenile_n = squirrels.filter(age='Juvenile').count()
+    #Shift
+    AM_n = squirrels.filter(shift='AM').count()
+    PM_n = squirrels.filter(shift='PM').count()
+    #Location 
+    above_ground_n = squirrels.filter(location='Above Ground')
+    ground_plane_n = squirrels.filter(location='Ground Plane')
+    #fur color
+    grey_n = squirrels.filter(primary_fur_color='Grey')
+    clinamon_n = squirrels.filter(primary_fur_color='Clinamon')
+    black_n = squirrels.filter(primary_fur_color = 'Black')
+
+    context = {
+            'total':total,
+            'Adult_n':Adult_n,
+            'Juvenile_n':Juvenile_n,
+            'AM_n':AM_n,
+            'PM_n':PM_n,
+            'above_ground_n':above_ground_n,
+            'ground_plane_n':ground_plane_n,
+            'grey_n':grey_n,
+            'clinamon_n':clinamon_n,
+            'black_n':black_n,
+            }
+
+    return render(request, 'sightings/stats.html',context)
+
+
 
 
 
