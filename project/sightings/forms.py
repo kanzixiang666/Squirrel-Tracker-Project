c43cee89a700644d2d21a49a765baebaee9a14a5
yxh <xy2469@columbia.edu>
2021-04-13 17:18:49 +0800

    xy2469 second commit

--- project/sightings/forms.py
+++ project/sightings/forms.py
@@ -6,7 +6,14 @@ from .models import Squirrel
 class Form(ModelForm):
     class Meta:
         model = Squirrel
-        fields = ['Latitude', 'Longitude', 'Shift', 'Date', 'Age']
+        fields = ['Latitude', 'Longitude', 'Squirrel_ID',
+                  'Shift', 'Date', 'Age',
+                  'Primary_Fur_Color', 'Location', 'Specific_Location',
+                  'Running', 'Chasing', 'Climbing',
+                  'Eating', 'Foraging', 'Other_Activities',
+                  'Kuks', 'Quaas', 'Moans',
+                  'Tail_Flags', 'Tail_Twitches', 'Approaches',
+                  'Indifferent', 'Runs_From']
 
 
 class CreateForm(ModelForm):
